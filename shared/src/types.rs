14e485baa39d042846a7b488f81956b83db4c134
Rusty Bee <145002912+rustybee42@users.noreply.github.com>
2026-04-01 13:50:23 +0200

    chore: put uid type into its own file

--- shared/src/types.rs
+++ shared/src/types.rs
@@ -6,100 +6,19 @@ use bee_serde_derive::BeeSerde;
 use core::hash::Hash;
 #[cfg(feature = "grpc")]
 use protobuf::beegfs as pb;
-#[cfg(feature = "sqlite")]
-use rusqlite::ToSql;
-#[cfg(feature = "sqlite")]
-use rusqlite::types::FromSql;
 use std::fmt::{Debug, Display};
 use std::str::FromStr;
-use std::sync::Arc;
 
 mod entity;
 pub use entity::*;
+mod uid;
+pub use uid::*;
 
 // Type aliases for convenience. Used by BeeGFS messaging and the management.
 //
 // CAUTION: While most known BeeGFS messages use the aliased integers below for these types, some
 // do not. It still has to be checked for each BeeGFS message individually which exact type is
 // needed for serialization.
-
-#[derive(Debug, Default, Clone, Eq)]
-pub struct Uid {
-    uid: i64,
-    info: Option<Arc<str>>,
-}
-
-impl Uid {
-    pub fn with_info(uid: i64, info: impl Into<Arc<str>>) -> Self {
-        Self {
-            uid,
-            info: Some(info.into()),
-        }
-    }
-
-    pub fn raw(&self) -> i64 {
-        self.uid
-    }
-}
-
-impl PartialEq for Uid {
-    fn eq(&self, other: &Self) -> bool {
-        self.uid == other.uid
-    }
-}
-
-impl PartialOrd for Uid {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.uid.partial_cmp(&other.uid)
-    }
-}
-
-impl Hash for Uid {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.uid.hash(state);
-    }
-}
-
-impl Display for Uid {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if let Some(ref info) = self.info {
-            Display::fmt(info, f)
-        } else {
-            write!(f, "uid:{}", self.uid)
-        }
-    }
-}
-
-impl From<i64> for Uid {
-    fn from(value: i64) -> Self {
-        Uid {
-            uid: value,
-            info: None,
-        }
-    }
-}
-impl From<Uid> for i64 {
-    fn from(value: Uid) -> Self {
-        value.uid
-    }
-}
-
-#[cfg(feature = "sqlite")]
-impl FromSql for Uid {
-    fn column_result(value: rusqlite::types::ValueRef) -> rusqlite::types::FromSqlResult<Self> {
-        Ok(Uid {
-            uid: value.as_i64()?,
-            info: None,
-        })
-    }
-}
-
-#[cfg(feature = "sqlite")]
-impl ToSql for Uid {
-    fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {
-        self.uid.to_sql()
-    }
-}
 
 pub type TargetId = u16;
 pub type BuddyGroupId = u16;
@@ -109,7 +28,6 @@ pub type PoolId = u16;
 pub type QuotaId = u32;
 
 pub const MGMTD_ID: NodeId = 1;
-pub const MGMTD_UID: Uid = Uid { uid: 1, info: None };
 pub const DEFAULT_STORAGE_POOL: PoolId = 1;
 
 /// The BeeGFS node type
