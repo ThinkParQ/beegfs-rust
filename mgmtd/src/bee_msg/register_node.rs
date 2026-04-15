dda621d93c36c81257ae66c23eaf6ea546d96082
Philipp Falk <80761729+philippfalk@users.noreply.github.com>
2026-04-15 15:47:23 +0200

    feat(license): disallow client mounts when no license loaded

--- mgmtd/src/bee_msg/register_node.rs
+++ mgmtd/src/bee_msg/register_node.rs
@@ -2,13 +2,17 @@ use super::*;
 use common::update_node;
 use shared::bee_msg::node::*;
 
+const REGISTERNODEMSG_COMPATFLAG_CLIENT_SUPPORTS_REGREJ: u8 = 1;
+
 impl HandleWithResponse for RegisterNode {
     type Response = RegisterNodeResp;
 
     async fn handle(self, app: &impl App, _req: &mut impl Request) -> Result<Self::Response> {
         fail_on_pre_shutdown(app)?;
 
-        let node_id = update_node(self, app).await?;
+        let reject = (_req.msg_compat_feature_flags() & REGISTERNODEMSG_COMPATFLAG_CLIENT_SUPPORTS_REGREJ) != 0;
+
+        let node_id = update_node(self, app, reject).await?;
 
         let fs_uuid: String = app
             .read_tx(|tx| db::config::get(tx, db::config::Config::FsUuid))
